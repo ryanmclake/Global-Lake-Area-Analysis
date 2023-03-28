6d11efec421b56e22ca4177ca946791bf2377b18
Ryan McClure <rmcclure@CIS-RMcclure-Air.local>
2023-03-28 13:47:18 -0600

    UPDATED GLCP

---
+++ scripts/PC_scripts/D2_glcp_slice_add_kendall_tau.R
@@ -0,0 +1,50 @@
+rm(list=ls())
+
+# script calculates kendall tau on GLCP slim data set 
+  # glcp slim is the GLCP 2.0 with only the columns of interest
+  #script:
+     # selects columns 
+     # calculates kendall tau
+     # joins to original data set
+     # exports
+
+# =======================================================================
+#------------------------------------------------------------------------
+#### initial time for script start #### 
+
+s = Sys.time()
+
+# Elegant way to quickly install packages fast
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(vroom, dplyr, tidyr, Kendall, readr)
+
+#### Bringing in 'glcp slim' data set ####
+d <- vroom::vroom("./output/D1_glcp_yearly_slice.csv")
+
+#### Calculating kendall tau for each lake ####
+
+k <- d %>% 
+#selecting columns
+  select(hylak_id, permanent_km2) %>%
+  group_by(hylak_id) %>%
+  summarise(across(c(1),  ~list(MannKendall(.) %>%
+                                  tidy %>%
+                                  select(p.value, statistic)))) %>%
+#unnest so p.value and kendall tau statistic are separate columns
+  ungroup(.) %>%
+  unnest(c(2), names_repair = "minimal") %>%
+#rename 'statistic' to 'kendall_tau'
+  rename(kendall_tau = statistic) %>%
+#remove 'p.value' column
+  select(-p.value)
+
+#### Joining and exporting ####
+#join
+left_join(d, k, by = "hylak_id") %>%
+#export
+  write_csv(., file = paste0("./output/D2_glcp_slice_added_kendall_tau.csv"))
+
+#### Time check ####
+e <- Sys.time()
+t=e-s
+print(t)
