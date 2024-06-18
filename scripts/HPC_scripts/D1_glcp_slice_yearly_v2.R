33baa1bfbd05d1f7067d07a63aed5943a2d27088
Ryan McClure <mcclurer@battelleecology.org>
2024-06-18 15:20:26 -0600

    update slice code with windows parallel option

--- scripts/HPC_scripts/D1_glcp_slice_yearly_v2.R
+++ scripts/HPC_scripts/D1_glcp_slice_yearly_v2.R
@@ -33,42 +33,55 @@ library(vroom, warn.conflicts = FALSE)
 library(feather, warn.conflicts = FALSE)
 library(parallel, warn.conflicts = FALSE)
 library(doParallel, warn.conflicts = FALSE)
+library(snow, warn.conflicts = FALSE)
 
-file <-  list.files(path = "/central/groups/carnegie_poc/rmcclure/GLCP-2.0/GLCP_2.0_official/")
-
+#file <-  list.files(path = "/central/groups/carnegie_poc/rmcclure/GLCP-2.0/GLCP_2.0_official/")
+file <-  list.files(path = "C:/Users/mcclurer/OneDrive - National Ecological Observatory Network/Documents/R/Global-Lake-Area-Analysis/inputs/countries")
   #imports using 'vroom' 
   #BE SURE TO UPDATE YOUR PATH
 
 slice_glcp <- function(x){
 
 # vroom::vroom(paste0("/central/groups/carnegie_poc/rmcclure/GLCP-2.0/GLCP_2.0_official/",file)) %>%
-d <- vroom::vroom(paste0("/central/groups/carnegie_poc/rmcclure/GLCP-2.0/GLCP_2.0_official/", file)) 
+d <- vroom::vroom(paste0("C:/Users/mcclurer/OneDrive - National Ecological Observatory Network/Documents/R/Global-Lake-Area-Analysis/inputs/countries/", file), col_names = F) 
 
-m <- vroom::vroom("/central/groups/carnegie_poc/rmcclure/GLCP-2.0/basin_matches.csv")
+m <- vroom::vroom("C:/Users/mcclurer/OneDrive - National Ecological Observatory Network/Documents/R/Global-Lake-Area-Analysis/inputs/matches/basin_matches.csv") |>
+  rename(X1 = HYBAS_ID)
 
 d %>%
-    left_join(., m, by = "HYBAS_ID") %>%
-    group_by(Hylak_id, year, Center_Lat, Center_Lon, Country) %>%
+    left_join(., m, by = "X1") %>%
+    group_by(X3, X2, X9, X10, X7) %>%
     #choose what year you want to start from
     
     #filter(year>=1995) %>%
-    filter(year>=2000) %>%
+    filter(X2>=2000) %>%
     
     #summarizing observations at each lake by year 
     #slicing the first value by this grouping since it is the same for the whole year
     slice(1) %>%
     #BE SURE TO UPDATE YOUR PATH
-    write.table(., file = paste0("/central/groups/carnegie_poc/rmcclure/GLCP-2.0/outputs/D1_glcp_yearly_slice.csv"),
-                   append = T,
-                   row.names = F,
-                   col.names = !file.exists("/central/groups/carnegie_poc/rmcclure/GLCP-2.0/outputs/D1_glcp_yearly_slice.csv"))
+    write.table(., file = paste0("C:/Users/mcclurer/OneDrive - National Ecological Observatory Network/Documents/R/Global-Lake-Area-Analysis/D1_glcp_yearly_slice.csv"),
+              append = T,
+              row.names = F,
+              col.names = !file.exists("C:/Users/mcclurer/OneDrive - National Ecological Observatory Network/Documents/R/Global-Lake-Area-Analysis/D1_glcp_yearly_slice.csv"))
+  
+    # write.table(., file = paste0("/central/groups/carnegie_poc/rmcclure/GLCP-2.0/outputs/D1_glcp_yearly_slice.csv"),
+    #                append = T,
+    #                row.names = F,
+    #                col.names = !file.exists("/central/groups/carnegie_poc/rmcclure/GLCP-2.0/outputs/D1_glcp_yearly_slice.csv"))
 
 }
 
-no_cores <- detectCores()
-cl <- makeCluster(no_cores, type="FORK")
-registerDoParallel(cl)
-foreach(x=file) %dopar% slice_glcp(x)
+no_cores <- detectCores() - 2
+cl<-makeCluster(no_cores, type="SOCK")
+system.time(clusterApply(cl, file, slice_glcp(file)))
+stopCluster(cl)
+
+
+# no_cores <- detectCores()
+# cl <- makeCluster(no_cores, type="FORK")
+# registerDoParallel(cl)
+# foreach(x=file) %dopar% slice_glcp(x)
 
 #### Time check ####
 e <- Sys.time()
